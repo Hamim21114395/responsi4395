56f0ac0c38a5141c4ed6c104096cfe349f5e4e52
hamim <hamimnurkhamid.students.amikom.ac.id>
2022-07-12 12:27:28 +0700

    raw

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ProjectPenjualan
 {
     class Program
     {
         // deklarasi objek collection untuk menampung objek penjualan
-        static List<string> daftarPenjualan = new List<string>();
+        public static List<Penjualan> list = new List<Penjualan>();
 
         static void Main(string[] args)
         {
@@ -18,7 +16,7 @@ namespace ProjectPenjualan
             {
                 TampilMenu();
 
-                Console.Write("\nNomor Menu [1..4]: ");
+                Console.Write("\nNomor Menu [1..3]: ");
                 int nomorMenu = Convert.ToInt32(Console.ReadLine());
 
                 switch (nomorMenu)
@@ -43,16 +41,30 @@ namespace ProjectPenjualan
         static void TampilMenu()
         {
             Console.Clear();
-
-            // PERINTAH: lengkapi kode untuk menampilkan menu
+            Console.WriteLine("Pilihan menu:");
+            Console.WriteLine();
+            Console.WriteLine("1. Tambah Penjualan");
+            Console.WriteLine("2. Tampilkan Penjualan");
+            Console.WriteLine("3. Keluar");
         }
 
         static void TambahPenjualan()
         {
             Console.Clear();
-
             // PERINTAH: lengkapi kode untuk menambahkan penjualan ke dalam collection
-
+            Console.WriteLine("Tambah Data Penjualan\n");
+            Console.Write("Nota: ");
+            Penjualan pjl = new Penjualan();
+            pjl.Nota = Console.ReadLine();
+            Console.Write("Tanggal: ");
+            pjl.Tanggal = Console.ReadLine();
+            Console.Write("Customer: ");
+            pjl.Customer = Console.ReadLine();
+            Console.Write("Jenis [T/K]: ");
+            pjl.Jenis = Console.ReadLine() == "T" ? "Tunai" : "Kredit";
+            Console.Write("Total Nota: ");
+            pjl.TotalNota = Console.ReadLine();
+            list.Add(pjl);
             Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
             Console.ReadKey();
         }
@@ -60,8 +72,12 @@ namespace ProjectPenjualan
         static void TampilPenjualan()
         {
             Console.Clear();
+            int counter = 0;
 
-            // PERINTAH: lengkapi kode untuk menampilkan daftar penjualan yang ada di dalam collection
+            foreach (Penjualan pj in list)
+            {
+                Console.WriteLine($"{++counter}. {pj.Nota}, {pj.Tanggal}, {pj.Customer}, {pj.Jenis}, {pj.TotalNota}");
+            }
 
             Console.WriteLine("\nTekan enter untuk kembali ke menu");
             Console.ReadKey();
